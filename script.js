02cf67fb1e592a71a1f978ee73925985e0715b75
Yadvendra Shukla <yadvendrashukla919@gmail.com>
2022-10-05 08:59:04 +0530

    added some folders

---
+++ script.js
@@ -0,0 +1,24 @@
+let ham = document.getElementById("menu");
+// let cross = document.getElementsByClassName('cross');
+// let nav = document.querySelector("#navbar");
+
+AOS.init();
+
+// Using Jquery to define Scroll spy and set active class
+// $(document).ready(function () {
+//   $("#menu").click(function () {
+//     $(this).toggleClass("fa-times");
+//     $(".navbar").toggleClass("nav-toggle");
+//   });
+
+//   $(window).on("scroll load", function () {
+//     $("#menu").removeClass("fa-times");
+//     $(".navbar").removeClass("nav-toggle");
+
+//     if (window.scrollY > 60) {
+//       document.querySelector("#scroll-top").classList.add("active");
+//     } else {
+//       document.querySelector("#scroll-top").classList.remove("active");
+//     }
+
+   
