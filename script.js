8837630c63243c7ff6eba15e8f90b14a025c2775
Yadvendra Shukla <yadvendrashukla919@gmail.com>
2022-10-05 11:18:46 +0530

    fix hamburger scripting

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
 let ham = document.getElementById("menu");
-// let cross = document.getElementsByClassName('cross');
-// let nav = document.querySelector("#navbar");
+let nav = document.querySelector(".navbarGo");
 
-AOS.init();
 
-// Using Jquery to define Scroll spy and set active class
-// $(document).ready(function () {
-//   $("#menu").click(function () {
-//     $(this).toggleClass("fa-times");
-//     $(".navbar").toggleClass("nav-toggle");
-//   });
-
-//   $(window).on("scroll load", function () {
-//     $("#menu").removeClass("fa-times");
-//     $(".navbar").removeClass("nav-toggle");
-
-//     if (window.scrollY > 60) {
-//       document.querySelector("#scroll-top").classList.add("active");
-//     } else {
-//       document.querySelector("#scroll-top").classList.remove("active");
-//     }
-
-   
+ham.addEventListener("click", ()=>{
+nav.classList.toggle('navbarGo');
+ham.classList.toggle('fa-times');
+})
